c3e4cb73d10b401a310523675067f7da33376ee5
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-06-04 07:43:33 -0500

    Implement Add todo item

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,25 +1,27 @@
 import { useState } from 'react';
+import { v4 as uuidv4 } from "uuid";
 import InputTodo from '@/components/InputTodo';
 import TodosList from '@/components/TodosList';
 
 const TodosLogic = () => {
   const [todos, setTodos] = useState([
     {
-      id: 1,
+      id: uuidv4(),
       title: 'Setup development environment',
       completed: true,
     },
     {
-      id: 2,
+      id: uuidv4(),
       title: 'Develop website and add content',
       completed: false,
     },
     {
-      id: 3,
+      id: uuidv4(),
       title: 'Deploy to live server',
       completed: false,
     },
   ]);
+
   const handleChange = (id) => {
     setTodos((prevState) =>
       prevState.map((todo) => {
@@ -33,12 +35,27 @@ const TodosLogic = () => {
       })
     );
   };
+
   const delTodo = (id) => {
-    console.log('deleted', id);
+    setTodos([
+      ...todos.filter((todo) => {
+        return todo.id !== id;
+      }),
+    ]);
   };
+
+  const addTodoItem = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+  
   return (
     <div>
-      <InputTodo />
+      <InputTodo addTodoItem={addTodoItem} />
       <TodosList
         todosProps={todos}
         handleChange={handleChange}
